24f8aa3cbf6baea66a469a78ca73feda39d7af54
Ricardo Lopez <rlopez@next-ecommerce.com>
2024-07-18 00:59:58 +0200

    Install React-Boostrap || PropTypes

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import Header from "../header/Header";
+import PropTypes from "prop-types";
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Header />
+			{children}
+		</>
+	);
+};
+
+Layout.propTypes = {
+	children: PropTypes.node.isRequired,
+};
+
+export default Layout;

96b6eeb681b6d7e9c87bb760d6e6b1cd0350df6c
Adrián S <adriansotes@gmail.com>
2024-07-25 16:53:30 +0200

    feat: login

---
+++ src/pages/auth/service.jsx
@@ -0,0 +1,23 @@
+import {
+  client,
+  setAuthorizationHeader,
+  removeAuthorizationHeader,
+} from "../../api/client";
+import storage from "../utils/storage";
+
+export const login = async (email, password) => {
+  const credentials = {
+    email,
+    password,
+  };
+
+  return client.post("user/login", credentials).then(({ token }) => {
+    setAuthorizationHeader(token);
+    storage.set("authToken", token);
+  });
+};
+
+export const logout = () => {
+  removeAuthorizationHeader();
+  storage.remove("auth");
+};

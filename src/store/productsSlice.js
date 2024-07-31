0b0355908781175fbefee9fc4662d4d289680076
Jorge Q. <jqb94@hotmail.com>
2024-08-01 01:54:17 +0200

    Adding error control and redirection to product page

--- src/store/productsSlice.js
+++ src/store/productsSlice.js
@@ -1,41 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { createProduct } from './productsAsyncThunk';
+import { createProduct } from './productsThunk';
 
 const productDefaultState = {
-  products: {
     loaded: false,
     data: [],
-  },
-  ui: {
     pending: false,
     error: null,
-  },
 };
 
 const productsSlice = createSlice({
   name: 'products',
-  initialState: productDefaultState, // Importa el estado inicial
+  initialState: productDefaultState, 
   reducers: {
+    clearError: (state) => {
+      state.error = null;
+    }
   },
   extraReducers: (builder) => {
     builder
       .addCase(createProduct.pending, (state) => {
-        state.ui.pending = true;
-        state.ui.error = null; 
+        state.pending = true;
+        state.error = null; 
       })
       .addCase(createProduct.fulfilled, (state, action) => {
-        state.products.data.push(action.payload); // Agrega el nuevo producto
-        state.ui.pending = false;
-        state.ui.error = null; 
+        state.data.push(action.payload); 
+        state.pending = false;
+        state.error = null; 
       })
       .addCase(createProduct.rejected, (state, action) => {
-        state.ui.pending = false;
-        state.ui.error = action.payload; // Guarda el error
-      });
+        state.pending = false;
+        state.error = action.payload; // Guarda el error
+      })
   },
 });
 
-// No se necesita exportar las acciones síncronas
-// export const { action } = productsSlice.actions;
+
+export const { clearError } = productsSlice.actions;
 export default productsSlice.reducer;
 

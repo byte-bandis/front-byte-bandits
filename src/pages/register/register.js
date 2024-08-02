ae7f803cff33adade72c01cd001c0f180ba0b8a1
Adrián S <adriansotes@gmail.com>
2024-08-02 11:14:11 +0200

    chore: deleting unused code in register.js

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -9,16 +9,3 @@ export const register = (userData) => {
     }
   });
 };
-
-/* export const register = (userData) => {
-  return client.post("user/register", userData).then(({ token, user }) => {
-    if (token) {
-      setAuthorizationHeader(token);
-      storage.set("auth", token);
-      return {
-        message: "User created correctly",
-        user,
-      };
-    }
-  });
-}; */

6aba9781db1a179ba6a1e71e4a2c2ddef7f07339
Adrián S <adriansotes@gmail.com>
2024-08-05 18:53:08 +0200

    fix: rmove useEffect in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./components/layout/Layout";
 import RootRouter from "./routes/RootRouter";
 import BreadCrumb from "./components/breadcrumb/BreadCrumb";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { setRememberMe } from "./store/loginThunk";
 import storage from "./utils/storage";
 import { setAuthorizationHeader } from "./api/client";
 

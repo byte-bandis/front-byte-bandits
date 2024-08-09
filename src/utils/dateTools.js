f1fa784fe45158f3853bfe616072baadc455e594
Adrián S <adriansotes@gmail.com>
2024-08-09 13:55:16 +0200

    chore: change default date for date trimmer

--- src/utils/dateTools.js
+++ src/utils/dateTools.js
@@ -1,4 +1,4 @@
-export function trimDate(date = "xxxx-xx-xx", outputFormat = "EN") {
+export function trimDate(date = "0000-00-00", outputFormat = "EN") {
   const trimmer = new RegExp("^\\d{4}-\\d{2}-\\d{2}");
   const trimmedDateEN = date.match(trimmer)[0];
   const [year, month, day] = trimmedDateEN.split("-");

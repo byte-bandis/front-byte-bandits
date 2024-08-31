0dcb3e4c75c57357945f492c231174faf003cdd3
Adrián S <adriansotes@gmail.com>
2024-08-31 10:59:32 +0200

    feat: validations for address set and working

--- src/pages/customer/myPersonalInfo/components/paymentValidations.js
+++ src/pages/customer/myPersonalInfo/components/paymentValidations.js
@@ -6,7 +6,7 @@ export const validate = ({ creditCard }) => {
       newErrors.creditCard = "Credit card number must be 13 to 18 digits";
     }
   }
-  console.log("Esto es creditCard: ", creditCard);
+
   if (creditCard !== "card") {
     if (!/^\d*$/.test(creditCard)) {
       newErrors.creditCard = "Credit card number must contain only digits";

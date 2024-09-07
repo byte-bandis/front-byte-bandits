cfcebd259256c9021f15317a630dc1d0adc69505
Santiagomadaw <santiagoma.daw@gmail.com>
2024-09-07 19:31:12 +0200

    Envio correo al hacer registro.

--- src/pages/register/service.js
+++ src/pages/register/service.js
@@ -3,6 +3,11 @@ import { client } from "../../api/client";
 export const register = (userData) => {
   return client.post("user/register", userData).then(({ token }) => {
     if (token) {
+      client.post("nodemailer/",  {
+        email: userData.email,
+        userName: userData.username,
+        type: "welcome",
+      });
       return {
         message: "User created correctly",
       };

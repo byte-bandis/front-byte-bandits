935e1cc26fc6295770e643b65dc21f71720bd39a
Santiagomadaw <santiagoma.daw@gmail.com>
2024-09-11 20:34:51 +0200

    rehago el refactor de adrian de ListAdStyle

--- src/components/shared/lists/StyledAdList.jsx
+++ src/components/shared/lists/StyledAdList.jsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 const StyledAdList = styled.div`
-  margin: ${(props) => props.$customMargin || "12% auto"};
+  margin: ${(props) => props.$customMargin || "auto"};
   display: grid;
+  position: relative;
+  top: ${(props) => props.$customTop || "0px"};
   justify-content: space-between;
   justify-items: center;
   width: 80%;
+ max-width: calc(280px * 5);
   gap: 30px;
-  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
   padding-top: 0px;
 
   &:has(.no-ad[noad]) {
@@ -19,6 +22,7 @@ const StyledAdList = styled.div`
     text-wrap: nowrap;
     text-align: start;
   }
+    
 `;
 
 export default StyledAdList;
--- src/pages/product/ProductList.jsx
+++ src/pages/product/ProductList.jsx
@@ -3,12 +3,12 @@ import propTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getAds } from "../../store/adsThunk";
-import styled from "styled-components";
+
 import Pager from "../pagination/Pager";
 import { getWishlist } from "../../store/likesThunk";
 import ErrorMessage from "./components/ErrorMessage";
 import { resetMessage } from "../../store/uiSlice";
-
+import { StyledAdList } from "../../components/shared/lists";
 const ProductList = ({ $customMargin, $customTop }) => {
   const dispatch = useDispatch();
   const page = useSelector((state) => state.adsState.page);
@@ -82,28 +82,5 @@ ProductList.propTypes = {
   $customMargin: propTypes.string,
   $customTop: propTypes.string,
 };
-const StyledAdList = styled.div`
-  margin: ${(props) => props.$customMargin || "auto"};
-  display: grid;
-  position: relative;
-  top: ${(props) => props.$customTop || "0px"};
-  justify-content: space-between;
-  justify-items: center;
-  width: 80%;
- max-width: calc(280px * 5);
-  gap: 30px;
-  grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
-  padding-top: 0px;
 
-  &:has(.no-ad[noad]) {
-    display: flex;
-  }
-
-  .no-ad {
-    color: silver;
-    text-wrap: nowrap;
-    text-align: start;
-  }
-    
-`;
 export default ProductList;

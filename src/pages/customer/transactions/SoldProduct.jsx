4e20efd169975475fa04eb0838f18374b02e5d90
Santiagomadaw <santiagoma.daw@gmail.com>
2024-09-18 18:25:28 +0200

    filtro desde el backend

--- src/pages/customer/transactions/SoldProduct.jsx
+++ src/pages/customer/transactions/SoldProduct.jsx
@@ -1,7 +1,7 @@
 import StyledMyAccount from "../../../components/shared/StyledMyAccount";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { getTransactionsByUser } from "../../../store/transactionsThunk";
+import { getCountSellerTransactions, getTransactionsByUser } from "../../../store/transactionsThunk";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import ListItems from "../../product/components/ListItems";
@@ -33,34 +33,27 @@ const SoldProducts = () => {
   );
 
   const [showSoldProducts, setShowSoldProducts] = useState(true);
-
-  const userSeller = adsData
-    .filter((item) => item.user._id === userid)
-    .filter((item) => item.sell === true);
-
-  const userSellerSoldProducts = transactionsData
-    .filter((item) => item.seller)
-    .filter((item) => item.seller._id === userid);
-
   useEffect(() => {
     dispatch(getTotalAds({ user: username }));
     const allFilters = { ...filters, page, limit };
 
     if (username) {
       allFilters.user = username;
+      allFilters.sell = true
     }
     dispatch(getAds({ id: "", filters: allFilters }));
   }, [dispatch, filters, limit, page, username]);
 
   const adsAccount = useSelector((state) => state.adsState.totalAds);
   const transactionsAccount = useSelector(
-    (state) => state.transactions.totalTransactions,
+    (state) => state.transactions.count,
   );
+
 
   useEffect(() => {
     if (userid) {
-      dispatch(getTransactionsByUser({ page, limit }));
-      dispatch(getTransactionsByUser());
+      dispatch(getTransactionsByUser({ filters: {  seller: true, page, limit } }));
+      dispatch(getCountSellerTransactions());
     }
   }, [dispatch, limit, page, userid]);
 
@@ -80,11 +73,11 @@ const SoldProducts = () => {
         <div>
           {showSoldProducts ? (
             <>
-              {userSeller.length > 0 ? (
+              {adsData.length > 0 ? (
                 <>
                   <ListItems
-                    username={userSeller.map((item) => item.user._id)}
-                    data={userSeller}
+                    username={adsData.map((item) => item.user._id)}
+                    data={adsData}
                     ItemContiner={ProductItem}
                   />
                   <Pager adsAccount={adsAccount} limit={4} page={1} />
@@ -95,11 +88,11 @@ const SoldProducts = () => {
             </>
           ) : (
             <>
-              {userSellerSoldProducts.length > 0 ? (
+              {transactionsData.length > 0 ? (
                 <>
                   <ListItems
-                    data={userSellerSoldProducts}
-                    username={userSellerSoldProducts.map((item) => item._id)}
+                    data={transactionsData}
+                    username={transactionsData.map((item) => item._id)}
                     ItemContiner={TransactionItem}
                   />
                   <Pager adsAccount={transactionsAccount} limit={4} page={1} />
